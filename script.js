e49d9cd53970c6d379b0dc9715b4df679b95d36c
Rafael Terra <rterragd@hotmail.com>
2022-08-13 20:53:20 -0300

    reply comments working

--- script.js
+++ script.js
@@ -1,190 +1,189 @@
 class Comment {
-  constructor(id, img, nickname, date, content, rating, replies) {
+  constructor(id, user, date, content, score, replies) {
     this.id = id;
-    this.img = img;
-    this.nickname = nickname;
-    this.date = date;
+    this.user = user;
+    this.createdAt = date;
     this.content = content;
-    this.rating = rating;
+    this.score = score;
     this.replies = replies;
   }
-  element(type, parent_id) {
-    //create comment element
-    let new_comment_element = document.createElement('div');
-    if (type == 'reply_1') {
-      new_comment_element.classList.add('comment-box', 'first-reply');
-      new_comment_element.dataset.parentId = parent_id;
+  createCommentBox(context_id) {
+    if (this.replies != undefined) {
+      let new_comment_context = document.createElement('div');
+      new_comment_context.classList.add('comment-context');
+      new_comment_context.dataset.context = this.id;
+
+      new_comment_context.innerHTML = `<div class="default-box" data-comment="main"><div class="inner-comment-box" data-comment-id=${this.id}></div></div>`;
+
+      document
+        .querySelector('#comments-section-wrapper')
+        .appendChild(new_comment_context);
     } else {
-      new_comment_element.classList.add('main-comment-wrapper');
+      let new_reply_box = document.createElement('div');
+      new_reply_box.classList.add('default-box');
+      // new_reply_box.dataset.contextId = this.id;
+      new_reply_box.dataset.comment = 'reply';
+      new_reply_box.innerHTML = `<div class='inner-comment-box' data-context='${context_id}' data-comment-id=${this.id}></div>`;
+      document
+        .querySelector(`.comment-context[data-context='${context_id}']`)
+        .appendChild(new_reply_box);
     }
-    new_comment_element.dataset.id = this.id;
-    let inner_comment;
-    if (type == 'main') {
-      inner_comment = document.createElement('div');
-      inner_comment.classList.add('comment-box');
-      inner_comment.dataset.id = this.id;
-      new_comment_element.appendChild(inner_comment);
-    }
-    //create rating section
+
+    /*Create a rating buttons element as a inner wrapper flex child*/
     let rating_section = document.createElement('div');
     rating_section.classList.add('rating');
     rating_section.dataset.id = this.id;
-    type == 'reply_1' && rating_section.classList.add('is-reply');
     rating_section.innerHTML =
       '<div onclick=Comment.updateScore() role="button" data-rating="+">+</div><div class="current-rating">' +
-      this.rating +
+      this.score +
       '</div><div onclick=Comment.updateScore() role="button" data-rating="-">-</div>';
 
-    //create main content section
+    document
+      .querySelector(`.inner-comment-box[data-comment-id='${this.id}']`)
+      .appendChild(rating_section);
+
+    /*Create a main content element as a inner wrapper flex child*/
     let main_content_section = document.createElement('div');
     main_content_section.classList.add('main-content');
     main_content_section.innerHTML =
       '<div class="comment-header" data-id=' +
       this.id +
       '><img src=" ' +
-      this.img +
+      this.user.image.png +
       ' " alt=""><span class="nickname">' +
-      this.nickname +
+      this.user.username +
       '</span><span class="date">' +
-      this.date +
-      '</span><span data-active=false class="reply-btn" role="button" onclick=Comment.addReply(' +
+      this.createdAt +
+      '</span><span data-active=false class="reply-btn" role="button" onclick=Comment.createReplyComposerBox(' +
       this.id +
       ')><img src="./images/icon-reply.svg">Reply</span></div><div class="comment-content">' +
       this.content +
       '</div>';
-
-    //inserts a class='inner' wrapper div to use flexbox
-    if (type == 'main') {
-      inner_comment.appendChild(document.createElement('div'));
-      inner_comment.querySelector('div').classList.add('inner');
-    } else if (type == 'reply_1') {
-      new_comment_element.appendChild(document.createElement('div'));
-      new_comment_element.querySelector('div').classList.add('inner');
-    }
-
-    //appends flex child rating section
-    new_comment_element.querySelector('div.inner').appendChild(rating_section);
-
-    //appends flex child main content section
-    new_comment_element
-      .querySelector('div.inner')
+    document
+      .querySelector(`.inner-comment-box[data-comment-id='${this.id}']`)
       .appendChild(main_content_section);
+  }
+  static createReplyComposerBox(comment_id) {
+    if (event.target.dataset.active == 'false') {
+      let target_comment = document.querySelector(
+        `.inner-comment-box[data-comment-id="${comment_id}"]`,
+      );
+      // target_comment.hasAttribute('data-context') && console.log('reply');
 
-    //appends the comment component
-    //if its a main comment, appends to main comments section wrapper
-    //else if its a reply, appends to its reference comment wrapper
-    if (type == 'main') {
-      document
-        .querySelector('#comments-section-wrapper')
-        .appendChild(new_comment_element);
-    } else if (type == 'reply_1') {
+      let reply_composer_box = document.createElement('div');
+      reply_composer_box.classList.add(
+        'default-box',
+        'reply-composer',
+        'comment-composer',
+      );
+      reply_composer_box.dataset.repliedComment = comment_id;
+      let n = target_comment.hasAttribute('data-context')
+        ? target_comment.dataset.context
+        : comment_id;
+      reply_composer_box.innerHTML = `<div class='inner-reply'><div class='avatar'><img src=${commentsObj.currentUser.image.png}></div><div class='reply-input'><textarea name='reply-input' rows='4'></textarea></div><div class='reply-send-btn'><button onclick=Comment.submitReply(${n})>Send</button></div></div>`;
+
+      reply_composer_box.style.width =
+        target_comment.parentElement.offsetWidth - 30 + 'px';
+
+      target_comment.parentElement.parentElement.insertBefore(
+        reply_composer_box,
+        target_comment.parentElement.nextElementSibling,
+      );
+
+      event.target.dataset.active = true;
+    } else {
       document
         .querySelector(
-          `.main-comment-wrapper[data-id="${new_comment_element.dataset.parentId}"]`,
+          `.comment-composer[data-replied-comment='${comment_id}']`,
         )
-        .appendChild(new_comment_element);
+        .remove();
+      event.target.dataset.active = false;
     }
   }
   static updateScore() {
-    switch (event.target.dataset.rating) {
-      case '+':
-        if (event.target.parentElement.classList.contains('is-reply')) {
-          let parent_id =
-            event.target.parentElement.parentElement.parentElement.dataset
-              .parentId;
-          let cmm_id = event.target.parentElement.dataset.id;
-          commentsObj.comments.forEach((mainComment) => {
-            if (mainComment.id == parent_id) {
-              mainComment.replies.forEach((r) => {
-                if (r.id == cmm_id) r.score += 1;
-              });
-            }
-          });
-          event.target.parentElement.querySelector(
-            '.current-rating',
-          ).innerText =
-            +event.target.parentElement.querySelector('.current-rating')
-              .innerText + 1;
-        } else {
-          commentsObj.comments[
-            +event.target.parentElement.dataset.id - 1
-          ].score += 1;
-        }
-        break;
-      case '-':
-        if (event.target.parentElement.classList.contains('is-reply')) {
-          let parent_id =
-            event.target.parentElement.parentElement.parentElement.dataset
-              .parentId;
-          let cmm_id = event.target.parentElement.dataset.id;
-          commentsObj.comments.forEach((mainComment) => {
-            if (mainComment.id == parent_id) {
-              mainComment.replies.forEach((r) => {
-                if (r.id == cmm_id) r.score -= 1;
-              });
-            }
-          });
-          event.target.parentElement.querySelector(
-            '.current-rating',
-          ).innerText =
-            +event.target.parentElement.querySelector('.current-rating')
-              .innerText - 1;
-        } else {
-          commentsObj.comments[
-            +event.target.parentElement.dataset.id - 1
-          ].score -= 1;
-        }
-        break;
-    }
-    localStorage.setItem('commentsObj', JSON.stringify(commentsObj));
-    if (!event.target.parentElement.classList.contains('is-reply')) {
+    let rating_id = event.target.parentElement.dataset.id;
+    if (
+      !event.target.parentElement.parentElement.hasAttribute('data-context')
+    ) {
+      commentsObj.comments[+rating_id - 1].score +=
+        event.target.dataset.rating == '+' ? 1 : -1;
+
+      /* Update score */
       event.target.parentElement.querySelector('.current-rating').innerText =
         commentsObj.comments[+event.target.parentElement.dataset.id - 1].score;
-    }
-  }
-  static addReply() {
-    if (event.target.dataset.active == 'false') {
-      let target_comment = document.querySelector(
-        `.comment-box[data-id="${event.target.parentElement.dataset.id}"]`,
-      );
-      let reply_box = document.createElement('div');
-      reply_box.classList.add('comment-box');
-      reply_box.classList.add('reply-composer');
-
-      event.target.dataset.active = true;
-
-      //defines if the reply box has a smaller size
-      target_comment.classList.contains('first-reply') &&
-        reply_box.classList.add('reply-box');
-
-      if (target_comment.dataset.parentId) {
-        refComment = target_comment.dataset.parentId;
-      } else {
-        refComment = target_comment.dataset.id;
-      }
-
-      reply_box.innerHTML =
-        "<div class='inner-reply'><div class='avatar'><img src=" +
-        commentsObj.currentUser.image.png +
-        "></div><div class='reply-input'><textarea name='reply-input' rows='4'></textarea></div><div class='reply-send-btn'><button onclick=Comment.submitReply()>Send</button></div></div>";
-      
-
-      target_comment.parentElement.insertBefore(
-        reply_box,
-        target_comment.nextSibling,
-      );
     } else {
-      event.target.dataset.active = false;
-      document
-        .querySelector(
-          `.comment-box[data-id="${event.target.parentElement.dataset.id}"]`,
-        )
-        .nextElementSibling.remove();
+      let _context = event.target.parentElement.parentElement.dataset.context;
+      commentsObj.comments.forEach((mainComment) => {
+        if (mainComment.id == _context) {
+          mainComment.replies.forEach((r) => {
+            if (r.id == rating_id) {
+              r.score += event.target.dataset.rating == '+' ? 1 : -1;
+              /* Update score */
+              event.target.parentElement.querySelector(
+                '.current-rating',
+              ).innerText = r.score;
+            }
+          });
+        }
+      });
     }
+    localStorage.setItem('commentsObj', JSON.stringify(commentsObj));
   }
-  static submitReply() {}
+  static submitReply(reply_to, replied_comment) {
+    let ref_comment = document.querySelector(
+      `.inner-comment-box[data-comment-id='${reply_to}']`,
+    );
+    // ref_comment.hasAttribute('data-context') && console.log('vai mention');
+    let comment_content =
+      event.target.parentElement.parentElement.querySelector('textarea').value;
+
+    // let comment_content = `${
+    //   '@user' +
+    //   event.target.parentElement.parentElement.querySelector('textarea').value
+    // } `;
+
+    let comment_createdAt = () => {
+      // let now = new Date();
+      // return now.getTime();
+      return 'today';
+    };
+
+    let submmited_comment_object = {
+      id: (idCounter += 1),
+      content: comment_content,
+      createdAt: comment_createdAt(),
+      score: 0,
+      replyingTo: ref_comment.querySelector('.nickname').innerText,
+      user: commentsObj.currentUser,
+    };
+    let new_reply = new Comment(
+      submmited_comment_object.id,
+      submmited_comment_object.user,
+      submmited_comment_object.createdAt,
+      submmited_comment_object.content,
+      submmited_comment_object.score,
+    );
+
+    new_reply.createCommentBox(reply_to);
+
+    commentsObj.comments.forEach((mainComment) => {
+      if (mainComment.id == reply_to) {
+        mainComment.replies.push(new_reply);
+      }
+    });
+    localStorage.setItem('commentsObj', JSON.stringify(commentsObj));
+
+    event.target.parentElement.parentElement.parentElement.remove();
+
+    document.querySelector(
+      `.inner-comment-box .reply-btn`,
+    ).dataset.active = false;
+  }
 }
 
+//
+//create a global comments object from JSON or from localStorage
+let idCounter = 0;
 let commentsObj;
 if (localStorage.commentsObj) {
   commentsObj = JSON.parse(localStorage.commentsObj);
@@ -197,34 +196,34 @@ if (localStorage.commentsObj) {
       renderElements(commentsObj);
     });
 }
+//END create a global comments object from JSON or from localStorage
+//
 
+//
+//function that renders comments elements from Comment class
 function renderElements(obj) {
   obj.comments.forEach((comment) => {
+    idCounter += 1;
     let new_comment = new Comment(
       comment.id,
-      comment.user.image.png,
-      comment.user.username,
+      comment.user,
       comment.createdAt,
       comment.content,
       comment.score,
       comment.replies.length,
     );
-    new_comment.element('main');
+    new_comment.createCommentBox();
 
     //check if the comment has replies to render
     if (comment.replies.length > 0) {
-      let parent_id = comment.id;
+      let context_id = comment.id;
       comment.replies.forEach((r) => {
-        let reply = new Comment(
-          r.id,
-          r.user.image.png,
-          r.user.username,
-          r.createdAt,
-          r.content,
-          r.score,
-        );
-        reply.element('reply_1', parent_id);
+        idCounter += 1;
+        let reply = new Comment(r.id, r.user, r.createdAt, r.content, r.score);
+        reply.createCommentBox(context_id);
       });
     }
   });
 }
+//END function that renders comments elements from Comment class
+//
